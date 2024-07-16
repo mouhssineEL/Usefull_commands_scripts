beb67ae5f74c544c15db110c0fec2189ef5c9074
mouhssineEL <81980061+mouhssineEL@users.noreply.github.com>
2024-07-16 15:30:22 +0200

    Add files via upload

---
+++ unique_processes.ps1
@@ -0,0 +1,17 @@
+﻿$processes = Get-Process | Sort-Object Name, StartTime
+
+$uniqueProcesses = @{}
+
+foreach ($process in $processes) {
+    if (-not $uniqueProcesses.ContainsKey($process.Name)) {
+        $parent = Get-Process -Id $process.Id -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Parent -ErrorAction SilentlyContinue
+        $uniqueProcesses[$process.Name] = [PSCustomObject]@{
+            Name = $process.Name
+            Id = $process.Id
+            StartTime = $process.StartTime
+            ParentId = if ($parent) { $parent.Id } else { $null }
+        }
+    }
+}
+
+$uniqueProcesses.Values | Sort-Object StartTime | Format-Table -Property Name, Id, StartTime, ParentId -AutoSize
